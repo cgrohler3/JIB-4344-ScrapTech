ff057b29b77c2fec82e1dd658a695f9412dadcae
CamNoName <cgrohler3@gatech.edu>
2024-11-15 10:53:02 -0500

    Database Init

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const admin = require('firebase-admin');
+const serviceAcc = require('./firebaseConfig/servAcc.json');
 require('dotenv').config;
 
+//init firestore
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAcc),
+})
+
+const db = admin.firestore();
 //server named 'app'
 const app = express();
 //port 3000 for development
@@ -14,3 +22,8 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+
+
+
+module.exports = db;
