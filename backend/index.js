d503519782c3f0ead418bd357c3bfdd5b1bda5f6
CamNoName <cgrohler3@gatech.edu>
2024-11-15 06:43:23 -0500

    Added cors and dotenv
    Included dependencies useful for making requests from front end as well as accessing env vars.

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,16 @@
-console.log('Hello World!');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config;
+
+//server named 'app'
+const app = express();
+//port 3000 for development
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Help!!!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
