b0ffdd8f64bdf530bd5903c87206a7741c317c64
rsmith611 <rsmith611@gatech.edu>
2024-11-16 22:43:24 -0500

    test commit

--- frontend/src/ScrapApp/app/(tabs)/donations.js
+++ frontend/src/ScrapApp/app/(tabs)/donations.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from 'react-native';
 const HomeScreen = () => {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Donations</Text>
+            <Text style={styles.title}>Donation</Text>
         </View>
     );
 };
