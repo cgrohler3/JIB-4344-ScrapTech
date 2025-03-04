2e012f373a97a39b0943e062239ef166c07d1f91
CamNoName <cgrohler3@gatech.edu>
2025-03-04 15:04:36 -0500

    Populate Heat Map
    Heat map makes a query to pull data from firestore, building the heat map array and displaying on the map

--- frontend/src/ScrapApp/app/(auth)/(tabs)/dataMap.js
+++ frontend/src/ScrapApp/app/(auth)/(tabs)/dataMap.js
@@ -1,13 +1,22 @@
 import { StyleSheet, Text, View } from 'react-native'
 import MapView, {Heatmap, PROVIDER_GOOGLE} from 'react-native-maps'
+import {collection, getDocs} from "firebase/firestore";
+import React, {useState, useEffect} from 'react'
+import { db } from '../../../lib/firebaseConfig'
 
-import React, {useState} from 'react'
-
-const heatmapData = [
-	{ latitude: 33.750, longitude: -84.3885, weight: 1 },  //this is how points are set, weight is point size
-	{ latitude: 33.750, longitude: -84.4, weight: 1 }, //and lat/long is the location of the point. Will need to
-	{ latitude: 37.79025, longitude: -84.3865, weight: 1 }, //match these to zip codes I guess
-]
+const getZipPos = async () => {
+	const zipPos = collection(db, "zip_positions")
+	const snapshot = await getDocs(zipPos)
+	const data = snapshot.docs.map(doc => {
+		const docData = doc.data()
+		return {
+			latitude: docData.lat,
+			longitude: docData.long,
+			weight: docData.total_weight
+		}
+	})
+	return data
+}
 
 const dataMap = () => {
 	const [region, setRegion] = useState({
@@ -17,59 +26,70 @@ const dataMap = () => {
 		longitudeDelta: 0.05,  //this is the region and zoom, coords are set for ATL, GA
 	})
 
+	const [heatmapData, setHeatmapData] = useState([])
 
-  return (
-    <View style={styles.container}>
-        <Text style={styles.title}>Heat Map</Text>
-		<View style={styles.mapContainer}>
-			<MapView
+	useEffect(() => {
+		const getmapData = async() => {
+			const data = await getZipPos()
+			setHeatmapData(data)
+		}
+		getmapData()
+	}, [])
+
+	return (
+		<View style={styles.container}>
+		  <Text style={styles.title}>Heat Map</Text>
+		  <View style={styles.mapContainer}>
+			{heatmapData.length > 0 ? (
+			  <MapView
 				style={styles.map}
 				region={region}
 				onRegionChangeComplete={setRegion}
-				provider= {PROVIDER_GOOGLE}   //sets where map comes from, google right now
-			>
+				provider={PROVIDER_GOOGLE} //sets where map comes from, google right now
+			  >
 				<Heatmap
-						points={heatmapData} 
-						opacity={0.65}
-						radius={50}
-						gradient={{
-							colors: ["green", "yellow", "red"],
-							startPoints: [0.05, 0.25, 0.5],
-							colorMapSize: 50
-						}}
-					/>
-			</MapView>
+				  points={heatmapData}
+				  opacity={0.65}
+				  radius={50}
+				  gradient={{
+					colors: ["green", "yellow", "red"],
+					startPoints: [0.05, 0.25, 0.5],
+					colorMapSize: 50,
+				  }}
+				/>
+			  </MapView>
+			) : (
+			  <Text>Loading Map...</Text>
+			)}
+		  </View>
 		</View>
-    </View>
-
-  )
-}
-
-
-export default dataMap
-
-const styles = StyleSheet.create({
-    container: {
+	  );
+	};
+	
+	export default dataMap;
+	
+	const styles = StyleSheet.create({
+	  container: {
 		flex: 1,
 		justifyContent: 'flex-start',
 		alignItems: 'center',
 		backgroundColor: '#f5f5f5',
 		paddingTop: 80,
 		paddingHorizontal: 20,
-	},
-	mapContainer: {
+	  },
+	  mapContainer: {
 		flex: 1,
 		width: '100%',
 		borderColor: '#376c3e',
 		borderWidth: 4,
-	},
-	map: {
+	  },
+	  map: {
 		flex: 1,
 		width: '100%',
-	},
-	title: {
+	  },
+	  title: {
 		fontSize: 25,
 		fontWeight: 'bold',
 		color: '#376c3e',
-	}
-})
+	  },
+	});
