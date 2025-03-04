b42ecd68b9240be03fed804e42efcbf527df00e2
CamNoName <cgrohler3@gatech.edu>
2025-03-04 14:08:06 -0500

    Map Tweaks
    Swapped code to ensure datapoints are declared and updated provider to PROVIDER_GOOGLE

--- frontend/src/ScrapApp/app/(auth)/(tabs)/dataMap.js
+++ frontend/src/ScrapApp/app/(auth)/(tabs)/dataMap.js
@@ -1,7 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
-import MapView, {Heatmap} from 'react-native-maps';
+import MapView, {Heatmap, PROVIDER_GOOGLE} from 'react-native-maps'
 
 import React, {useState} from 'react'
+
+const heatmapData = [
+	{ latitude: 33.750, longitude: -84.3885, weight: 1 },  //this is how points are set, weight is point size
+	{ latitude: 33.750, longitude: -84.4, weight: 1 }, //and lat/long is the location of the point. Will need to
+	{ latitude: 37.79025, longitude: -84.3865, weight: 1 }, //match these to zip codes I guess
+]
 
 const dataMap = () => {
 	const [region, setRegion] = useState({
@@ -9,7 +15,7 @@ const dataMap = () => {
 		longitude: -84.3885,
 		latitudeDelta: 0.05,
 		longitudeDelta: 0.05,  //this is the region and zoom, coords are set for ATL, GA
-	});
+	})
 
 
   return (
@@ -20,21 +26,24 @@ const dataMap = () => {
 				style={styles.map}
 				region={region}
 				onRegionChangeComplete={setRegion}
-				provider='google'   //sets where map comes from, google right now
+				provider= {PROVIDER_GOOGLE}   //sets where map comes from, google right now
 			>
-				<Heatmap points={heatmapData} />
+				<Heatmap
+						points={heatmapData} 
+						opacity={0.65}
+						radius={50}
+						gradient={{
+							colors: ["green", "yellow", "red"],
+							startPoints: [0.05, 0.25, 0.5],
+							colorMapSize: 50
+						}}
+					/>
 			</MapView>
 		</View>
     </View>
 
   )
 }
-
-const heatmapData = [
-	{ latitude: 33.7501, longitude: -84.3885, weight: 1 },  //this is how points are set, weight is point size
-	{ latitude: 37.78855, longitude: -84.3855, weight: 10 }, //and lat/long is the location of the point. Will need to
-	{ latitude: 37.79025, longitude: -84.3865, weight: 14 }, //match these to zip codes I guess
-];
 
 
 export default dataMap
