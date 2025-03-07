e162221ba3d3f7a1118906b151b41d81019863c0
Hmiller62 <haydenmiller242@gmail.com>
2025-03-07 14:32:41 -0500

    UI Updates, new features

--- frontend/src/ScrapApp/app/(auth)/(tabs)/dataMap.js
+++ frontend/src/ScrapApp/app/(auth)/(tabs)/dataMap.js
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import MapView, {Heatmap, PROVIDER_GOOGLE} from 'react-native-maps'
+import MapView, {Heatmap, PROVIDER_GOOGLE, Polygon, Marker, Callout} from 'react-native-maps'
 import {collection, getDocs} from "firebase/firestore";
 import React, {useState, useEffect, useCallback} from 'react'
 import { db } from '../../../lib/firebaseConfig'
@@ -67,16 +67,34 @@ const dataMap = () => {
 				onRegionChangeComplete={setRegion}
 				provider={PROVIDER_GOOGLE} //sets where map comes from, google right now
 			  >
+				<Marker coordinate={{ latitude: 33.7490, longitude: -84.3880 }}>
+					<Callout>
+						<View>
+							<Text>Atlanta</Text>
+						</View>
+					</Callout>  {/*We could use markers for store locations/cities in future*/}
+				</Marker>
 				<Heatmap
 				  points={heatmapData}
-				  opacity={0.65}
+				  opacity={0.75}
 				  radius={50}
 				  gradient={{
-					colors: ["green", "yellow", "red"],
-					startPoints: [0.05, 0.25, 0.5],
+					colors: ["green", "blue", "red"],
+					startPoints: [0.05, 0.25, 0.50],
 					colorMapSize: 50,
 				  }}
 				/>
+				  <Polygon
+					  coordinates={[  //Changing lat goes up/down, changing long goes left/right
+						  { latitude: 33.6500, longitude: -84.5000 }, // Bottom-left
+						  { latitude: 33.6500, longitude: -84.2500 }, // Bottom-right
+						  { latitude: 33.8250, longitude: -84.2500 }, // Top-right
+						  { latitude: 33.8250, longitude: -84.5000 }, // Top-left
+					  ]}
+					  strokeColor="#376c3e" //Polygon lets us potentially highlight specific zip codes in future.
+					  fillColor="rgba(144, 238, 144, 0.4)" //For now I just have it highlighting ATL.
+					  strokeWidth={1}
+				  />
 			  </MapView>
 			) : (
 			  <Text>Loading Map...</Text>
@@ -122,31 +140,43 @@ const dataMap = () => {
 		width: '100%',
 	  },
 	  title: {
+		paddingBottom: 10,
 		fontSize: 25,
 		fontWeight: 'bold',
 		color: '#376c3e',
 	  },
 	  reloadButton: {
+		borderRadius: 3,
 		paddingTop: 20,
 		paddingBottom: 20,
-		backgroundColor: 'black',
-		width: 100
+		marginTop: 5,
+		backgroundColor: '#376c3e',
+		width: 100,
+		height: 55
 	  },
 	  reloadText: {
-		fontSize: 16,
-		textAlign: 'center',
-		color: 'white'
+		  fontSize: 18,
+		  fontWeight: 'bold',
+		  textAlign: 'center',
+		  color: 'white',
+		  textAlignVertical: 'center',
+		  lineHeight: 18
 	  },
 	  toggleButton: {
+		borderRadius: 3,
 		paddingTop: 20,
-		paddingBottom: 20,
-		backgroundColor: 'black',
-		width: 120
+		marginTop: 5,
+		backgroundColor: '#376c3e',
+		width: 120,
+		height: 55
 	  },
 	  toggleText: {
-		fontSize: 16,
+		fontSize: 18,
+		fontWeight: 'bold',
 		textAlign: 'center',
-		color: 'white'
+		color: 'white',
+		textAlignVertical: 'center',
+		lineHeight: 18
 	  },
 	  lowerView: {
 		flexDirection: 'row',
