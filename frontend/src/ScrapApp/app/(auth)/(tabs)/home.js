42cb8bffe7ba142c7d03305646c2a59bb477e3c9
CamNoName <cgrohler3@gatech.edu>
2025-03-30 14:32:28 -0400

    Homescreen Info
    Added instructions from client as well as about the app in general to populate the home page and guide users.

--- frontend/src/ScrapApp/app/(auth)/(tabs)/home.js
+++ frontend/src/ScrapApp/app/(auth)/(tabs)/home.js
@@ -22,7 +22,20 @@ const HomeScreen = () => {
 					resizeMode="contain"
 				/>
 			</View>
-			<Text style={styles.title}>Welcome, {email}</Text>
+			<View>
+				<Text style={styles.title}>Welcome, {email}</Text>
+			</View>
+			<View style={styles.section}>
+				<Text style = {styles.insTitle}>***Remember***</Text>
+				<Text style = {styles.insBody}>+ Get BOTH <Text style={styles.grnTxt}>email address</Text> AND <Text style={styles.grnTxt}>name</Text> for donors who want <Text style={styles.grnTxt}>receipts</Text></Text>
+				<Text style={styles.insBody}>+ Zip Code is <Text style={styles.redTxt}>REQUIRED</Text></Text>
+			</View>
+			<View style={styles.section}>
+				<Text style={styles.insTitle}>First Time Logging Data?</Text>
+				<Text style={styles.insBody}>+ Select <Text style={styles.grnTxt}>Log Donation</Text> from the options below</Text>
+				<Text style={styles.insBody}>+ Enter data and Save Donation</Text>
+				<Text style={styles.insBody}>+ Double check that information is accurate</Text>
+			</View>
 		</View>
 	)
 }
@@ -44,6 +57,7 @@ const styles = StyleSheet.create({
 		fontSize: 25,
 		fontWeight: 'bold',
 		color: '#376c3e',
+		paddingBottom: 96
 	},
 	image: {
         width: 200,
@@ -58,6 +72,26 @@ const styles = StyleSheet.create({
 		gap: 32,
 		paddingTop: 64
 	},
+	insTitle: {
+		textAlign: 'center',
+		fontSize: 42,
+		paddingBottom: 16
+	},
+	insBody: {
+		textAlign: 'center',
+		fontSize: 20,
+		paddingBottom: 16
+	},
+	grnTxt: {
+		color: '#376c3e',
+		fontWeight: 'bold'
+	},
+	redTxt: {
+		color: 'red'
+	},
+	section: {
+		paddingBottom: 96
+	}
 })
 
 export default HomeScreen
