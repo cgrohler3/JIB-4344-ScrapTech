81fffd3ff3632776296799fd1a3f3cb82ae8f236
CamNoName <cgrohler3@gatech.edu>
2025-04-26 04:09:04 -0400

    Export Donations Function
    Added a button to export all donations from firebase to a csv file.

--- frontend/src/ScrapApp/app/(auth)/(tabs)/home.js
+++ frontend/src/ScrapApp/app/(auth)/(tabs)/home.js
@@ -2,11 +2,14 @@ import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'rea
 import React, { useState } from 'react'
 import { Linking } from 'react-native';
 
-
+import { db } from '../../../lib/firebaseConfig'
 import { auth } from '../../../lib/firebaseConfig'
 import { onAuthStateChanged } from 'firebase/auth'
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
-
+import { collection, getDocs } from 'firebase/firestore'
+import * as FileSystem from 'expo-file-system'
+import { shareAsync } from 'expo-sharing'
+import Papa from 'papaparse'
 const HomeScreen = () => {
 	const [email, setEmail] = useState('')
 	const openCalendly = () => {
@@ -14,6 +17,40 @@ const HomeScreen = () => {
 		Linking.openURL(calendlyUrl);
 	};
 
+	const exportDonos = async () => {
+		try {
+			const snapshot = await getDocs(collection(db, "donations"))
+			const formattedData = snapshot.docs.map(doc => {
+			  const data = doc.data();
+	
+			  //Timestamp formatted as a date
+			  const timestamp = data.timestamp ? data.timestamp.toDate() : null;
+			  const formattedDate = timestamp
+				? `${(timestamp.getMonth() + 1).toString().padStart(2, '0')}/${timestamp.getDate().toString().padStart(2, '0')}/${timestamp.getFullYear()}`
+				: "";
+		
+			  return {
+				Date: formattedDate,
+				"Full Name": data.name || "",
+				Email: data.email || "",
+				Zipcode: data.zipCode || "",
+				Weight: data.weight || "",
+				Category: data.category || "",
+			  };
+			});
+		
+			const csv = Papa.unparse(formattedData);
+			const fileUri = FileSystem.documentDirectory + 'donation-log.csv';
+			await FileSystem.writeAsStringAsync(fileUri, csv, {
+			  encoding: FileSystem.EncodingType.UTF8,
+			});
+
+			await shareAsync(fileUri);
+		
+		  } catch (error) {
+			console.error("Error exporting data:", error);
+		  }
+	}
 
 
 
@@ -41,6 +78,14 @@ const HomeScreen = () => {
 					style={styles.calButton}
 					onPress={openCalendly}>
 					<Text style={styles.calButtonText}>Open Calendly</Text>
+				</TouchableOpacity>
+			</View>
+			<View style = {styles.buttonContainer}>
+				<Text style={styles.calendarText}>Ready to Export?</Text>
+				<TouchableOpacity
+					style={styles.calButton}
+					onPress={exportDonos}>
+					<Text style={styles.calButtonText}>Export ALL Donos</Text>
 				</TouchableOpacity>
 			</View>
 		</View>
